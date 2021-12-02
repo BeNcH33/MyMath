4771dcd178ec93ab57f0a7227c58d8b331a9e055
BeNcH <slava.morzhakov@vk.com>
2021-12-02 22:29:05 +0300

    Добавьте файлы проекта.

---
+++ MyMath/MyMaths.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMath
+{
+    class MyMaths
+    {
+        public double Sin(double x, int nTerms = 15)
+
+        {
+
+            var Numerator = x * x * x;//Числитель
+
+            var factorialStart = 3;
+
+            double factorialResult = 6;
+
+            var result = x + Numerator / factorialStart;
+
+            var X = 1;
+
+            for (int i = 3; i <= nTerms; i++)
+
+            {
+
+                Numerator *= x * x;
+
+                factorialResult *= factorialStart * factorialStart - 1;
+
+                factorialStart += 2;
+
+                result += Numerator / factorialResult;
+
+                //X *= -1;
+
+            }
+
+            return result;
+
+        }
+    }
+}
+
---
+++ MyMath/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMaths myMath = new MyMaths();
+            int i = 0;
+            while (i < 15)
+            {
+                Console.WriteLine($"{myMath.Sin(1)}");
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}

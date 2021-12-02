3a19f785e46653bc2b94bb979317531702a2e87d
BeNcH <slava.morzhakov@vk.com>
2021-12-03 00:12:52 +0300

    Еще немного

---
+++ MyMath/MyFunction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMath
+{
+    class MyFunction
+    {
+        MyMaths MyMaths = new MyMaths();
+        public double InfCalc(double x)
+        {
+            if (x <= 0)
+            {
+                return MyMaths.Pow(MyMaths.Pow(MyMaths.Sin(x) - MyMaths.Ctn(x) + MyMaths.Ctn(x), 3), 2) + MyMaths.Pow(MyMaths.Sec(x) + MyMaths.Cos(x), 2);
+            }
+            else
+            {
+                return (((MyMaths.Pow(MyMaths.Ln(x), 2) - MyMaths.Log(2, x)) / (MyMaths.Log(3, x) * MyMaths.Log(2, x)))*MyMaths.Ln(x))*MyMaths.Log(10, MyMaths.Pow(x,2));
+            }
+        }
+        public double StandartMath(double x)
+        {
+            if (x <= 0)
+            {
+                return Math.Pow(Math.Pow(Math.Sin(x) - MyMaths.Ctn(x) + MyMaths.Ctn(x), 3), 2) + Math.Pow(MyMaths.Sec(x) + Math.Cos(x), 2);
+            }
+            else 
+            {
+                return (((Math.Pow(Math.Log(x), 2) - Math.Log(2, x)) / (Math.Log(3, x) * Math.Log(2, x))) * Math.Log(x)) * Math.Log(10, Math.Pow(x, 2));
+            }
+        }
+    }
+}
--- MyMath/MyMaths.cs
+++ MyMath/MyMaths.cs
@@ -81,7 +81,12 @@ namespace MyMath
 
         public double Ctn(double x)
         {
-            return Math.Round(Cos(x) / Sin(x), 2);
+            return Cos(x) / Sin(x);
+        }
+        public double Sec(double x)
+
+        {
+            return 1 / Cos(x);
         }
         public double Csc(double x)
 
@@ -92,6 +97,103 @@ namespace MyMath
 
         {
             return x >= 0 ? x : -x;
+        }
+        public double Ln(double power)
+
+        {
+
+            if (power == 1) return 0;
+
+            const double e = 2.71828182845905;
+
+            var p = power;
+
+            var n = 0.0;
+
+            double a;
+
+            while (p >= e)
+
+            {
+
+                p /= e;
+
+                n++;
+
+            }
+
+            n += (p / e);
+
+            p = power;
+
+            do
+
+            {
+
+                a = n;
+
+                var l = (p / (Exp(n - 1.0)));
+
+                var r = ((n - 1.0) * e);
+
+                n = ((l + r) / e);
+
+            } while (Abs(n - a) > 0.001);
+
+            return n;
+
+        }
+        public double Log(double x, double newBase)
+
+        {
+            return Ln(x) / Ln(newBase);
+        }
+        public double Pow(double x, int radix)
+
+        {
+            if (radix == 0) return 1;
+
+            var res = x;
+
+            var absRadix = Abs(radix);
+
+            for (int i = 1; i < absRadix; i++)
+
+            {
+                res *= x;
+            }
+            return radix > 0 ? res : 1 / res;
+        }
+        public double Exp(double n)
+
+        {
+
+            double x = n;
+
+            double p = 1.0 + x;
+
+            double fact = x;
+
+            double i = 1.0;
+
+            double l;
+
+            do
+
+            {
+
+                i++;
+
+                fact *= (x / i);
+
+                l = p;
+
+                p += fact;
+
+            } while (Abs(l - p) > 0.001);
+
+            return p;
+
         }
     }
 }
